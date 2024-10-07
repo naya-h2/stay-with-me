4511713936c6e6320124811d9d2ab5cfdb3e28d4
heewon <hw7666@sogang.ac.kr>
2024-10-08 01:58:47 +0900

    ✨ feat: login 화면 구현 완료

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,20 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ['./src/pages/**/*.{js,ts,jsx,tsx,mdx}', './src/components/**/*.{js,ts,jsx,tsx,mdx}', './src/app/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        main: {
+          mint: 'var(--main-mint)',
+        },
+        logo: {
+          green: 'var(--logo-green)',
+        },
+        Labels: {
+          Secondary: 'var(--Labels-Secondary)',
+        },
       },
     },
   },
