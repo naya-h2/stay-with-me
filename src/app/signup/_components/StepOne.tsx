2b77243acbc022a53a547a1f4e248e068c283142
heewon <hw7666@sogang.ac.kr>
2024-10-12 21:56:54 +0900

    💄 design: 회원가입 페이지 UI 구현

--- src/app/signup/_components/StepOne.tsx
+++ src/app/signup/_components/StepOne.tsx
@@ -1,25 +1,35 @@
+import { useFormContext } from 'react-hook-form';
+
 const INPUT_LIST = [
   {
-    label: '아이디',
-    placeholder: '아이디 입력',
+    id: 'email',
+    label: '이메일',
+    placeholder: '이메일 입력',
+    type: 'email',
   },
   {
+    id: 'password',
     label: '비밀번호',
     placeholder: '비밀번호 입력',
+    type: 'password',
   },
   {
+    id: 'nickname',
     label: '닉네임',
     placeholder: '닉네임 입력',
+    type: 'text',
   },
 ];
 
 function StepOne() {
+  const { register } = useFormContext();
+
   return (
     <div className="flex flex-col gap-8">
-      {INPUT_LIST.map(({ label, placeholder }) => (
-        <label key={label} className="font-semibold flex flex-col gap-[14px]">
+      {INPUT_LIST.map(({ label, placeholder, id, type }) => (
+        <label key={id} className="font-semibold flex flex-col gap-[14px]">
           {label}
-          <input placeholder={placeholder} />
+          <input type={type} placeholder={placeholder} {...register(id)} />
         </label>
       ))}
     </div>
